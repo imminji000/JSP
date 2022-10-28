ee68485c1736afeb2d6f9d492bb2f42659e97450
swlss7979 <swlss7979@gmail.com>
2022-10-28 12:49:05 +0900

    2022/10/28

---
+++ Shop/src/main/java/bean/OrderBean.java
@@ -0,0 +1,61 @@
+package bean;
+
+public class OrderBean {
+
+	private int orderNo;
+	private String orderID;
+	private int orderProduct;
+	private int orderCount;
+	private String orderDate;
+	
+	//추가필드
+	private String name;
+	private String prodName;
+	public int getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(int orderNo) {
+		this.orderNo = orderNo;
+	}
+	public String getOrderID() {
+		return orderID;
+	}
+	public void setOrderID(String orderID) {
+		this.orderID = orderID;
+	}
+	public int getOrderProduct() {
+		return orderProduct;
+	}
+	public void setOrderProduct(int orderProduct) {
+		this.orderProduct = orderProduct;
+	}
+	public int getOrderCount() {
+		return orderCount;
+	}
+	public void setOrderCount(int orderCount) {
+		this.orderCount = orderCount;
+	}
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProdName() {
+		return prodName;
+	}
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	
+	
+	
+	
+	
+}
