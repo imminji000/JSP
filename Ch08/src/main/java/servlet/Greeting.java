466dbbb3de2b13e9db65977651df457ad24b4365
swlss7979 <swlss7979@gmail.com>
2022-11-09 10:39:36 +0900

    2022/11/09

---
+++ Ch08/src/main/java/servlet/Greeting.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/greeting.do", "/greeting"})
+public class Greeting extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		//  해당 서블이 최초 실행될때 한번 호출
+		System.out.println("greeting init...");
+	}
+		
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//사용자의 요청방식이 GET일 경우 호출되는 메서드
+		System.out.println("greeting doGet...");
+			
+		resp.setContentType("text/html;charset=UTF-8");
+		
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html>");
+		writer.println("<head>");
+		writer.println("<meta charset='UTF-8'/>");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<h3>Hello 서블릿</h3>");
+		writer.println("<p>");
+		writer.println("<a href='/Ch08/1_서블릿.jsp'>1_서블릿</a><br/>");
+		writer.println("<a href='/Ch08/hello.do'>Hello 서블릿</a><br/>");
+		writer.println("<a href='/Ch08/welcome.do'>welcome 서블릿</a><br/>");
+		writer.println("<a href='/Ch08/greeting.do'>greeting 서블릿</a><br/>");
+		writer.println("</p>");
+		writer.println("</body>");
+		writer.println("</html>");
+		writer.close();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//요청방식이 POST일 경우 호출되는 메서드
+				System.out.println("greeting dopost");
+
+	}
+	
+	@Override
+	public void destroy() {
+		//서블잇이 종료될때 호출되는 메서드
+		System.out.println("greeting destroy...");
+	}
+}
